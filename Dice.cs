c816f2ab770c584bce8dc2e0be0d53c65bc8d48b
Sylvain Delépée <sylvain.delepee@gmail.com>
2019-09-26 13:09:08 +0200

    First Commit

---
+++ Dice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRolls
+{
+    public static class Dice
+    {
+        public static int Roll(int numberOfFaces)
+        {
+            Random random = new Random();
+            int result = random.Next(numberOfFaces) + 1;
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRolls
+{
+    class Program
+    {
+        static int score = 0;
+
+        static string input;
+
+        static int numberOfDices;
+        static int numberOfFaces;
+
+        static void Main(string[] args)
+        {
+            WelcomeScreen();
+        }
+
+        static void WelcomeScreen()
+        {
+            score = 0;
+            Console.Clear();
+            Console.WriteLine("Type in the dices to roll");
+            AnalyzeInput();
+        }
+
+        static void AnalyzeInput()
+        {
+            string dices = "";
+            string faces = "";
+            bool isReadingFaces = false;
+
+            input = Console.ReadLine();
+
+            foreach (char c in input)
+            {
+                if (char.Equals(c, 'd'))
+                {
+                    isReadingFaces = true;
+                }
+                else if (char.Equals(c, '+') || char.Equals(c, '-'))
+                {
+                    RollTheDice(dices, faces);
+                    dices = c.ToString();
+                    faces = "";
+                    isReadingFaces = false;
+                }
+                else if (char.IsDigit(c))
+                {
+                    if (isReadingFaces == false)
+                    {
+                        dices += c;
+                    }
+                    else faces += c;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                    Console.ReadLine();
+                    WelcomeScreen();
+                }
+            }
+
+            RollTheDice(dices, faces);
+            PrintResult();
+        }
+
+        static void RollTheDice(string dices, string faces)
+        {
+            int.TryParse(dices, out numberOfDices);
+            int.TryParse(faces, out numberOfFaces);
+
+            int result = 0;
+
+            for (int i = 0; i < Math.Abs(numberOfDices); i++)
+            {
+                result += Dice.Roll(numberOfFaces);
+            }
+            result *= Math.Sign(numberOfDices);
+            score += result;
+
+        }
+
+        static void PrintResult()
+        {
+            Console.WriteLine("You scored: " + score);
+            Console.ReadLine();
+            WelcomeScreen();
+        }
+    }
+}
